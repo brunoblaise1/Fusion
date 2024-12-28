e9e89d54a3eeec4b019f269e497b3b553ebd22b6
brunoblaise1 <boogier94@gmail.com>
2024-12-28 11:05:59 +0200

    Built a landing page ui not great yet and ihaving problems with z-index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -122,104 +122,73 @@ export default function Home() {
 
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* <canvas className="canvas">
-
-
-      </canvas> */}
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex bg-black text-zinc-100 overflow-hidden flex-col mx-auto h-screen w-full items-center text-center justify-center">
+      <div className=" absolute inset-0 bg-[radial-gradient(circle_at_center,transparent_0%,black_100%)]"/>
+      <div className="absolute inset-0 bg-gradient-to-b from-purple-800/20 " />
+      <div className="-translate-x-20 translate-y-16">
+      <div className="">
+          <div className="translate-x-4">
+            <div className="h-3 w-3 bg-red-200 rounded-full"></div>
+            <div className=" ml-2 h-3 w-3 bg-green-200 rounded-full"></div>
+            <div className="ml-2 h-3 w-3 bg-yellow-200 rounded-2xl"></div>
+           </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    
+        <div className="-translate-y-8 rotate-180">
+          <div className="">
+            <div className="h-3 w-3 bg-red-200 rounded-full"></div>
+            <div className=" ml-2 h-3 w-3 bg-green-200 rounded-full"></div>
+            <div className="ml-2 h-3 w-3 bg-yellow-200 rounded-2xl"></div>
+           </div>
+        </div>
+    
+      </div>
+      <h5 className="text-medium pb-8 font-semibold">Introducing</h5>
+      <h1 className="font-bold text-8xl pb-2">Fusions</h1>
+      <p className="text-[.8rem] pb-8 font-semibold">A programming language for guitar using guitar sound</p>
+     
+      <div className="bg-purple-300/20 p-2 w-96 h-49 rounded-xl md:w-[800px] md:h-[200px] ">
+        <div>
+           <Image src='/guitar.svg' height={30} width={63} alt="guitar" className="absolute -z-0 translate-y-1 -translate-x-36 rotate-12 md:hidden" />
+          <Image src='/guitar.svg' height={30} width={33} alt="guitar" className="absolute -z-0 translate-y-4 -translate-x-36 rotate-12     
+         
+         md:-translate-y-20 md:translate-x-2 md:ml-8 md:rotate-45
+         " />
+        </div>
+        
+        <div className="translate-y-30 -rotate-180 md:hidden">
+           <Image src='/guitar.svg' height={30} width={63} alt="guitar" className="absolute -z-0 translate-y-1 -translate-x-36 rotate-12 " />
+         <Image src='/guitar.svg' height={30} width={33} alt="guitar" className="absolute -z-0 translate-y-1 -translate-x-36 rotate-12 " />
+        </div>
+        <div>
+          <div className="flex ">
+            <div className="h-3 w-3 bg-red-500 rounded-full z-10"></div>
+            <div className="ml-2 h-3 w-3 bg-green-500 rounded-full z-10"></div>
+            <div className="ml-2 h-3 w-3 bg-yellow-500 rounded-2xl z-10"></div>
+           </div>
+        </div>
+
+        <div className="text-left p-1 text-zinc-50/10 ">
+          
+          <ul className="md:mt-3">
+            {[1, 2, 3, 4, 5, 6].map((i) => (
+              <div>
+              <div className="flex" >
+                <h1>{i}</h1>
+                <li key={i} className=" ml-2 border bg-purp-400/20 mt-3 h-0 w-full ">
+                   
+              </li>
+                </div>
+               
+              </div>
+                ))
+                
+            }
+
+          </ul>
+        </div>
+      </div>
+      
     </div>
   );
 }
