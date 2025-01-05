c91a59d2fe8785c22240a00b8728783631f82508
brunoblaise1 <boogier94@gmail.com>
2025-01-05 08:08:27 +0200

    guitar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
+   
         {children}
+
       </body>
     </html>
   );
